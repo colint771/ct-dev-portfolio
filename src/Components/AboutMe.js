4acc19952db5bc4a70f0ddda0923633c593e0266
colint771 <colint771@gmail.comgit config --global user.email colint771@gmail.com>
2023-03-27 20:45:44 -0400

    components for sections of website

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import './AboutMe.css';
-import avatar from './avatar.jpg';
+import React, { Component } from 'react';
 
-function AboutMe() {
-  return (
-    <section className="AboutMe">
-      <img src={avatar} alt="Avatar" />
-      <h2>About Me</h2>
-      <p>
-        Hi, I'm John Doe, a full-stack web developer. I have experience in HTML, CSS, JavaScript, React, Node.js, and more.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
-    </section>
-  );
+class About extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var profilepic= "images/"+this.props.data.image;
+      var bio = this.props.data.bio;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var resumeDownload = this.props.data.resumedownload;
+    }
+
+    return (
+      <section id="about">
+      <div className="row">
+         <div className="three columns">
+            <img className="profile-pic"  src={profilepic} alt="Nordic Giant Profile Pic" />
+         </div>
+         <div className="nine columns main-col">
+            <h2>About Me</h2>
+
+            <p>{bio}</p>
+            <div className="row">
+               <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="address">
+						   <span>{name}</span><br />
+                     <span>{phone}</span><br />
+                     <span>{email}</span><br />
+                     <span>{city}, {state}</span><br />
+                     <span>(willing to relocate)</span>
+					   </p>
+               </div>
+               <div className="columns download">
+                  <p>
+                     <a href={resumeDownload} className="button" target="_blank"><i className="fa fa-download"></i>Download Resume</a>
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+
+   </section>
+    );
+  }
 }
 
-export default AboutMe;
+export default About;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import './Portfolio.css';
-import project1 from './project1.jpg';
-import project2 from './project2.jpg';
-import project3 from './project3.jpg';
-import project4 from './project4.jpg';
-import project5 from './project5.jpg';
-import project6 from './project6.jpg';
+import React, { Component } from 'react';
 
-function Portfolio() {
-  return (
-    <section className="Portfolio">
-      <h2>Portfolio</h2>
-      <div className="ProjectList">
-        <div className="Project">
-          <img src={project1} alt="Project 1" />
-          <h3>Project 1</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <a href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
-          <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">Deployed Application</a>
+class Portfolio extends Component {
+  render() {
+
+    if (this.props.data) {
+      var projects = this.props.data.projects.map(function (projects) {
+        var projectImage = 'images/portfolio/' + projects.image;
+        
+        return (
+          <div key={projects.title} className="columns portfolio-item">
+            <div className="item-wrap">
+              <a href={projects.url} target="_blank" title={projects.title}>
+                <img alt={projects.title} src={projectImage} />
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                    <h5>{projects.title}</h5>
+                    <p>{projects.category}</p><br />
+                    <p>{projects.info}</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        )
+      })
+    }
+
+    return (
+      <section id="portfolio">
+
+        <div className="row">
+
+          <div className="twelve columns collapsed">
+
+            <h1>PROJECTS</h1>
+
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+              {projects}
+            </div>
+          </div>
         </div>
-      </div>  <div className="Project">
-        </div> 
-    </section>
-  )}
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
